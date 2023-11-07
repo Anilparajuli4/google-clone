c56234db015785152609f41245ead3428850bdd0
Anil parajuli <anilparajuli580@gmail.com>
2023-11-06 18:40:33 -0800

    creating footer component

---
+++ src/component/CountryLookUp.jsx
@@ -0,0 +1,14 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+function CountryLookUp() {
+  const [country, setCountry] = useState("");
+  useEffect(() => {
+    fetch(`https://extreme-ip-lookup.com/json/?key=${process.env.IP_API_KEY}`)
+      .then((res) => res.json())
+      .then((data) => setCountry(data));
+  }, []);
+  return <div>{country}</div>;
+}
+
+export default CountryLookUp;
---
+++ src/component/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import CountryLookUp from "./CountryLookUp";
+
+function Footer() {
+  return (
+    <footer className="absolute bottom-0 text-sm text-gray-500 bg-[#f2f2f2] w-full">
+      <div className="border-b px-8 py-3">
+        <CountryLookUp />
+      </div>
+      <div className="flex flex-col sm:flex-row justify-between items-center px-8 py-3 space-y-7 sm:space-y-0">
+        <ul className="flex items-center space-x-6">
+          <li className="link">About</li>
+          <li className="link">Advertising</li>
+          <li className="link">Business</li>
+          <li className="link">How search works</li>
+        </ul>
+        <ul className="flex items-center space-x-6">
+          <li className="link">Privacy</li>
+          <li className="link">Terms</li>
+          <li className="link">Settings</li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
