3119add3c7955e836b65c478b8923bf578a5a598
Anil parajuli <anilparajuli580@gmail.com>
2023-11-07 22:48:01 -0800

    add image result

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,7 +1,33 @@
+import ImageSearchResult from "@/component/ImageSearchResult";
+
+import Link from "next/link";
 import React from "react";
 
-function page() {
-  return <div>image</div>;
+async function WebImagePage({ searchParams }) {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`
+  );
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+  const data = await response.json();
+  const results = data.items;
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center p-10">
+        <h1 className="text-3xl">No results found</h1>
+        <p className="text-lg">
+          Try searching for something else or go back to the homepage
+          <Link href="/" className="text-blue-500 ml-2 hover:text-blue-700">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+
+  return <>{results && <ImageSearchResult results={data} />}</>;
 }
 
-export default page;
+export default WebImagePage;
---
+++ src/component/ImageSearchResult.jsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+function ImageSearchResult({ results }) {
+  return (
+    <div className="pb-24 mt-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4">
+        {results.items.map((result) => {
+          return (
+            <div key={result.link} className="mb-8">
+              <div className="group">
+                <Link href={result.image.contextLink}>
+                  <img
+                    src={result.link}
+                    alt={result.title}
+                    className="h-60 group-hover:shadow-xl w-full object-contain transition-shadow"
+                  />
+                </Link>
+                <Link href={result.image.contextLink}>
+                  <h2 className="group-hover:underline truncate text-xl">
+                    {result.title}
+                  </h2>
+                </Link>
+                <Link href={result.image.contextLink}>
+                  <p className="group-hover:underline text-gray-600">
+                    {result.displayLink}
+                  </p>
+                </Link>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default ImageSearchResult;
