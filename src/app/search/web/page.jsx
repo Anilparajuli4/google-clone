5ac083a85d3140cb75039be3eda35e594cb4aa36
Anil parajuli <anilparajuli580@gmail.com>
2023-11-07 16:42:12 -0800

    fetch data from google search api and shows the titles

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-function WebSearchPage() {
-  return <div>page</div>;
+async function WebSearchPage({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  const data = await response.json();
+  const results = data.items;
+
+  return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 }
 
 export default WebSearchPage;
