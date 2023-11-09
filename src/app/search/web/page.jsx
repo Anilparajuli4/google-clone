54be47cc1cfb7a25931cb2afdf9913da30723711
Anil parajuli <anilparajuli580@gmail.com>
2023-11-08 23:27:21 -0800

    env change

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 async function WebSearchPage({ searchParams }) {
   const startIndex = searchParams.start || "1";
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  // await new Promise((resolve) => setTimeout(resolve, 10000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${startIndex}`
   );
